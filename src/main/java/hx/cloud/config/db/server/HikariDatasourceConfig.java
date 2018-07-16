4dedf5e303d5b1a721e578c687073a5dd1b394d5
andyliang <2825523288@qq.com>
2018-07-16 22:46:06 +0800

    hikari数据源配置

--- src/main/java/hx/cloud/config/db/server/HikariDatasourceConfig.java
+++ src/main/java/hx/cloud/config/db/server/HikariDatasourceConfig.java
@@ -1,7 +1,6 @@
 package hx.cloud.config.db.server;
 
 import com.zaxxer.hikari.HikariDataSource;
-import com.zaxxer.hikari.metrics.MetricsTrackerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -9,11 +8,6 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-
-import javax.sql.DataSource;
-import java.util.Properties;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 
 /**
  * @Author mingliang
@@ -31,62 +25,63 @@ public class HikariDatasourceConfig {
     private String username;
     @Value("${spring.datasource.hikari.password}")
     private String password;
-    @Value("${spring.datasource.hikari.password}")
-    private long connectionTimeout;
-    @Value("${spring.datasource.hikari.password}")
-    private long validationTimeout;
-    @Value("${spring.datasource.hikari.password}")
-    private long idleTimeout;
-    @Value("${spring.datasource.hikari.password}")
-    private long leakDetectionThreshold;
-    @Value("${spring.datasource.hikari.password}")
-    private long maxLifetime;
-    @Value("${spring.datasource.hikari.password}")
-    private int maxPoolSize;
-    @Value("${spring.datasource.hikari.password}")
+    @Value("${spring.datasource.hikari.minimum-idle}")
     private int minimumIdle;
-    @Value("${spring.datasource.hikari.password}")
-    private long initializationFailTimeout;
-    @Value("${spring.datasource.hikari.password}")
-    private String connectionInitSql;
-    @Value("${spring.datasource.hikari.password}")
-    private String connectionTestQuery;
-    @Value("${spring.datasource.hikari.password}")
-    private String dataSourceClassName;
-    @Value("${spring.datasource.hikari.password}")
-    private String dataSourceJndiName;
-    @Value("${spring.datasource.hikari.password}")
-    private String poolName;
-    @Value("${spring.datasource.hikari.password}")
-    private String schema;
-    @Value("${spring.datasource.hikari.password}")
-    private String transactionIsolationName;
-    @Value("${spring.datasource.hikari.password}")
+    @Value("${spring.datasource.hikari.auto-commit}")
     private boolean isAutoCommit;
-    @Value("${spring.datasource.hikari.password}")
-    private boolean isReadOnly;
-    @Value("${spring.datasource.hikari.password}")
-    private boolean isIsolateInternalQueries;
-    @Value("${spring.datasource.hikari.password}")
+    @Value("${spring.datasource.hikari.maximum-pool-size}")
+    private int maxPoolSize;
+    @Value("${spring.datasource.hikari.idle-timeout}")
+    private long idleTimeout;
+    @Value("${spring.datasource.hikari.pool-name}")
+    private String poolName;
+    @Value("${spring.datasource.hikari.max-lifetime}")
+    private long maxLifetime;
+    @Value("${spring.datasource.hikari.connection-timeout}")
+    private long connectionTimeout;
+    @Value("${spring.datasource.hikari.connection-test-query}")
+    private String connectionTestQuery;
+    @Value("${spring.datasource.hikari.data-source-class-name}")
+    private String dataSourceClassName;
+    @Value("${spring.datasource.hikari.validation-timeout}")
+    private long validationTimeout;
+    @Value("${spring.datasource.hikari.leak-detection-threshold}")
+    private long leakDetectionThreshold;
+    @Value("${spring.datasource.hikari.initialization-fail-timeout}")
+    private long initializationFailTimeout;
+    @Value("${spring.datasource.hikari.connection-init-sql}")
+    private String connectionInitSql;
+    @Value("${spring.datasource.hikari.data-source-j-n-d-i}")
+    private String dataSourceJndiName;
+    @Value("${spring.datasource.hikari.schema}")
+    private String schema;
+    @Value("${spring.datasource.hikari.transaction-isolation}")
+    private String transactionIsolationName;
+    @Value("${spring.datasource.hikari.register-mbeans}")
     private boolean isRegisterMbeans;
-    @Value("${spring.datasource.hikari.password}")
-    private boolean isAllowPoolSuspension;
-    @Value("${spring.datasource.hikari.password}")
-    private DataSource dataSource;
-    @Value("${spring.datasource.hikari.password}")
-    private Properties dataSourceProperties;
-    @Value("${spring.datasource.hikari.password}")
-    private ThreadFactory threadFactory;
-    @Value("${spring.datasource.hikari.password}")
-    private ScheduledExecutorService scheduledExecutor;
-    @Value("${spring.datasource.hikari.password}")
-    private MetricsTrackerFactory metricsTrackerFactory;
-    @Value("${spring.datasource.hikari.password}")
-    private Object metricRegistry;
-    @Value("${spring.datasource.hikari.password}")
-    private Object healthCheckRegistry;
-    @Value("${spring.datasource.hikari.password}")
-    private Properties healthCheckProperties;
+    @Value("${spring.datasource.hikari.read-only}")
+    private boolean isReadOnly;
+
+//    @Value("${spring.datasource.hikari.password}")
+//    private boolean isIsolateInternalQueries;
+//    @Value("${spring.datasource.hikari.password}")
+//    private boolean isAllowPoolSuspension;
+//    @Value("${spring.datasource.hikari.password}")
+//    private DataSource dataSource;
+//    @Value("${spring.datasource.hikari.password}")
+//    private Properties dataSourceProperties;
+//    @Value("${spring.datasource.hikari.password}")
+//    private ThreadFactory threadFactory;
+//    @Value("${spring.datasource.hikari.password}")
+//    private ScheduledExecutorService scheduledExecutor;
+//    @Value("${spring.datasource.hikari.password}")
+//    private MetricsTrackerFactory metricsTrackerFactory;
+//    @Value("${spring.datasource.hikari.password}")
+//    private Object metricRegistry;
+//    @Value("${spring.datasource.hikari.password}")
+//    private Object healthCheckRegistry;
+//    @Value("${spring.datasource.hikari.password}")
+//    private Properties healthCheckProperties;
 
     @Bean(name = "hikariDataSource")
     @Qualifier("hikariDataSource")
@@ -96,8 +91,6 @@ public class HikariDatasourceConfig {
         hikariDataSource.setJdbcUrl(jdbcUrl);
         hikariDataSource.setUsername(username);
         hikariDataSource.setPassword(password);
-
-        hikariDataSource.setMinimumIdle(minimumIdle);
         return hikariDataSource;
     }
 
